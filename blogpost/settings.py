7b6efc0d92a7cac597d0158798c2aba7e29b0976
Ajam <ajamazamov@gmail.com>
2021-10-18 03:36:48 +0500

    modified for mobile

--- blogpost/settings.py
+++ blogpost/settings.py
@@ -5,6 +5,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+print(BASE_DIR)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -87,7 +88,7 @@ DATABASES = {
         'NAME': env('DB_NAME'),
         'USER': env('DB_USER'),
         'PASSWORD': env('DB_PASSWORD'),
-        'HOST': "database-2.cg92gydm3ia2.us-east-2.rds.amazonaws.com",
+        'HOST': 'localhost',   #"database-2.cg92gydm3ia2.us-east-2.rds.amazonaws.com",
         'PORT': env('DB_PORT'),
     }
 }
@@ -150,7 +151,8 @@ AUTHENTICATION_BACKENDS = [
     'allauth.account.auth_backends.AuthenticationBackend',
 ]
 SITE_ID = 1
-LOGIN_REDIRECT_URL = "/"
+ACCOUNT_CONFIRM_EMAIL_ON_GET  = True
+LOGIN_REDIRECT_URL = "home"
 LOGOUT_REDIRECT_URL = "/"
 ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_EMAIL_VERIFICATION = "mandatory"
